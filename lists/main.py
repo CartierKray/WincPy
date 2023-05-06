49e5163466f3df8d2cdaf44b409b38edcee4a073
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-06 17:10:57 +0200

    Part 1 Done

---
+++ lists/main.py
@@ -0,0 +1,15 @@
+# Do not modify these lines
+__winc_id__ = '6eb355e1a60f48a28a0bbbd0c88d9ab4'
+__human_name__ = 'lists'
+
+# Add your code after this line
+
+# Part 1 
+def alphabetical_order(film_names):
+    return sorted(film_names)
+
+films = ["Jurassic Park", "Star Wars", "E.T.", "Jaws"]
+sorted_films = alphabetical_order(films)
+print(sorted_films)
+
+# Part 2
