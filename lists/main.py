54c992657fc5eba2e9b90486a3cd75286fc3a0a7
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-06 17:25:05 +0200

    Finish

--- lists/main.py
+++ lists/main.py
@@ -4,6 +4,7 @@ __human_name__ = 'lists'
 
 # Add your code after this line
 
+
 # Part 1 
 def alphabetical_order(film_names):
     return sorted(film_names)
@@ -12,4 +13,43 @@ films = ["Jurassic Park", "Star Wars", "E.T.", "Jaws"]
 sorted_films = alphabetical_order(films)
 print(sorted_films)
 
+
 # Part 2
+def won_golden_globe(film_name):
+  golden_globe_winners = [
+        "jaws",
+        "star wars",
+        "e.t. the extra-terrestrial",
+        "schindler's list",
+        "memoirs of a geisha"
+    ]
+  return film_name.lower() in [movie.lower() for movie in golden_globe_winners]
+film = "Star Wars"
+is_won_golden_globe = won_golden_globe(film)
+print(is_won_golden_globe)
+
+
+# Part 3 
+def remove_toto_albums(album_list):
+       joseph_toto_albums = [
+        "Fahrenheit",
+        "The Seventh One",
+        "Kingdom of Desire",
+        "Tambu",
+        "Mindfields",
+        "Toto XX",
+        "Falling in Between",
+        "35th Anniversary – Live in Poland",
+        "Toto XIV",
+        "Old Is New"
+    ]
+       return [album for album in album_list if album not in joseph_toto_albums]
+albums = [
+    "Jurassic Park",
+    "Falling in Between",
+    "E.T.",
+    "The Seventh One",
+    "Jaws"
+]
+tidy_albums = remove_toto_albums(albums)
+print(tidy_albums)
