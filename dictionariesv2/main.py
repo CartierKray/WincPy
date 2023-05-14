0560f8535f9b6660611d6332704155b633eb4b41
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-14 10:41:28 +0200

    Part 1 & 2 Done

--- dictionariesv2/main.py
+++ dictionariesv2/main.py
@@ -5,3 +5,33 @@ __winc_id__ = "00a4ab32f1024f5da525307a1959958e"
 __human_name__ = "dictionariesv2"
 
 # Add your code after this line
+
+countries = get_countries()
+
+# Part 1
+def create_passport(name, date_of_birth, place_of_birth, height, nationality):
+    # Create a passport with the key value pairs. returing the arguments
+    passport = {
+            "name": name,
+            "date_of_birth": date_of_birth,
+            "place_of_birth": place_of_birth,
+            "height": height,
+            "nationality": nationality
+        }
+    
+    return passport
+
+
+# Part 2
+def add_stamp(passport, country):
+    if 'stamps' not in passport:
+        passport['stamps'] = []
+
+    if country != passport['nationality'] and country not in passport['stamps']:
+        passport['stamps'].append(country)
+
+    return passport
+
+
+
+# Part 3 
