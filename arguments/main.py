177291cacb3e2f22c4de7e124fd236c9759d2d6c
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-14 23:47:10 +0200

    Part 2 added

--- arguments/main.py
+++ arguments/main.py
@@ -10,3 +10,25 @@ def greet(name, greeting_template='Hello, <name>!'):
 
 # print(greet('Olya'))
 # print(greet('Anna', 'Whatsup, <name>!'))
+
+
+
+
+# Part 2
+def force(mass, body='earth'):
+    gravity_factors = {
+        'mercury': 3.7,
+        'venus': 8.9,
+        'earth': 9.8,
+        'moon': 1.6,
+        'mars': 3.7,
+        'jupiter': 23.1,
+        'saturn': 9.0,
+        'uranus': 8.7,
+        'neptune': 11.0,
+        'pluto': 0.7,
+    }
+    gravity = gravity_factors.get(body.lower(), 9.8)
+    return round(mass * gravity, 1)
+
+print(force(0.1, 'sun'))
