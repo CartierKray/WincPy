89be1fb09dffe30b16b3261f98a91ddfa3cb90d8
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-14 22:55:44 +0200

    Part 1 Done

--- arguments/main.py
+++ arguments/main.py
@@ -4,4 +4,9 @@ __human_name__ = 'arguments'
 
 # Add your code after this line
 
+# Part 1
+def greet(name, greeting_template='Hello, <name>!'):
+    return greeting_template.replace('<name>', name)
 
+# print(greet('Olya'))
+# print(greet('Anna', 'Whatsup, <name>!'))
