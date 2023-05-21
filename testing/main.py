96000563d3cfd4cf45e2e36edbd0d069040e31b4
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-21 21:10:15 +0200

    Part 2 Done

--- testing/main.py
+++ testing/main.py
@@ -2,3 +2,9 @@
 # 1
 def get_none():
     return None 
+
+
+# 2
+def flatten_dict(dictionary):
+    values = list(dictionary.values())
+    return values
--- testing/test_main.py
+++ testing/test_main.py
@@ -4,6 +4,18 @@ import main
 def test_get_none():
     result = main.get_none()
     assert result is None
-    print('Test Passed!')
+    print('Test #1 Passed!')
 
 test_get_none()
+
+
+
+# 2
+def test_flatten_dict():
+    dictionary = {'a': [42, 350], 'b': 3.14 }
+    expected_output = [[42, 350], 3.14]
+    result = main.flatten_dict(dictionary)
+    assert result == expected_output, f"Expected: {expected_output}, Got: {result}"
+    print('Test #2 Passed!')
+
+test_flatten_dict()
