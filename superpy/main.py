2d22f3f6f1285401dd89935f1982d38fe25afdba
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-24 15:27:44 +0200

    Add Code

--- superpy/main.py
+++ superpy/main.py
@@ -16,8 +16,61 @@ def main():
     subparsers = parser.add_subparsers(dest="command")
 
     # Command for buying
-    buy_parser = subparsers.add_parser("buy", )
+    buy_parser = subparsers.add_parser("buy", help="Buy a product")
+    buy_parser.add_argument("--product-name", required=True, help="Name of the product")
+    buy_parser.add_argument("--product-price", required=True, help="Price of the product")
+    buy_parser.add_argument("--expiration-date", required=True, help="Expiration date of the product")
 
+    # Command for selling
+    sell_parser = subparsers.add_parser("selling", help="Selling a product")
+    sell_parser.add_argument("--product-name", required=True, help="Name of the product")
+    sell_parser.add_argument("--product-price", required=True, help="Price of the product")
+
+    # Report command
+    report_parser = subparsers.add_parser("report", help="Generate reports")
+    report_parser.add_argument("type", choices=["inventory", "revenue", "profit"], help="Type of report to generate")
+    report_parser.add_argument("--date", help="Specify a specific date for the report")
+
+    # Advance time command
+    parser.add_argument("--advanced-time", type=int, help="The perceived date by a number of days")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Perform actions based on the provided arguments
+    if args.command == "buy":
+        # Implement the buying logic
+        product_name = args.product_name
+        product_price = args.product_price
+        expiration_date = args.expiration_date
+        # Write this to the "bought.csv" data file using csv.writer
+    elif args.command == "sell":
+        # Implement the selling logic
+        product_name = args.product_name
+        product_price = args.product_price
+        # Check if product is in stock
+        # if the product is available, write the data to the "sold.csv" data file using csv.writer
+        # if it's not available, a error message wilt display
+        
+    elif args.command == "report":
+        # Implement generating reports logic
+        report_type = args.type
+        report_date = args.date
+        if report_type == "inventory":
+            # Generate and display the inventory report
+            pass
+        if report_type == "revenue":
+            # Generate and display the inventory report
+            pass
+        if report_type == "profit":
+            #Generate and display the inventory report
+            pass
+
+        # Implement the logic for advancing the time
+        if args.advanced_time
+            # Read the current date from the text file
+            # Advance the date by the specified number of the days
+            # Write the updated date back to the text file
 
 
 if __name__ == "__main__":
