13cd647a3d347d181aff307761b8ed6ab4da6b4f
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-28 19:12:00 +0200

    Add Code

--- requests/main.py
+++ requests/main.py
@@ -3,3 +3,26 @@ __winc_id__ = "cc1b724762854e85a8defa04287f708b"
 __human_name__ = "requests"
 
 # Add your code after this line
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<p>Home, sweet home.</p>"
+
+
+@app.route("/greet/")
+def greet():
+    return "<h1>Hello, World!</h1>"
+
+
+@app.route("/greet/<name>")
+def greet_name(name):
+    return f"<h1>Hello, {name}!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
