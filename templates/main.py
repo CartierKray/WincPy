11382af7d198f3a68a8c12146245f78ed9d7fee4
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-30 14:57:53 +0200

    Add Code

--- templates/main.py
+++ templates/main.py
@@ -6,6 +6,14 @@ __human_name__ = "templates"
 app = Flask(__name__)
 
 
+from flask import Flask, render_template, redirect, url_for
+
+__winc_id__ = "9263bbfddbeb4a0397de231a1e33240a"
+__human_name__ = "templates"
+
+app = Flask(__name__)
+
+
 @app.route("/base")
 def base():
     title = "Base"
@@ -29,5 +37,5 @@ def about():
     return render_template("about.html", title=title)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
