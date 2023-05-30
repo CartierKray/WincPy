e0b2c7b0d736d728d49ea95d4c26edc299e583df
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-05-30 14:59:15 +0200

    Add Code

--- templates/main.py
+++ templates/main.py
@@ -6,8 +6,6 @@ __human_name__ = "templates"
 app = Flask(__name__)
 
 
-from flask import Flask, render_template, redirect, url_for
-
 __winc_id__ = "9263bbfddbeb4a0397de231a1e33240a"
 __human_name__ = "templates"
 
