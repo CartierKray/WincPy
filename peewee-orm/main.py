86ca3be23de0dd0f95028a2501e8a046cf91dd74
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-06-05 11:00:26 +0200

    Add

--- peewee-orm/main.py
+++ peewee-orm/main.py
@@ -8,6 +8,7 @@ __human_name__ = "Peewee ORM"
 
 def cheapest_dish() -> models.Dish:
     """You want ot get food on a budget
+
     Query the database to retrieve the cheapest dish available
     """
     ...
@@ -24,6 +25,7 @@ def vegetarian_dishes() -> List[models.Dish]:
 
 def best_average_rating() -> models.Restaurant:
     """You want to know what restaurant is best
+
     Query the database to retrieve the restaurant that has the highest
     rating on average
     """
@@ -32,6 +34,7 @@ def best_average_rating() -> models.Restaurant:
 
 def add_rating_to_restaurant() -> None:
     """After visiting a restaurant, you want to leave a rating
+
     Select the first restaurant in the dataset and add a rating
     """
     ...
@@ -39,6 +42,7 @@ def add_rating_to_restaurant() -> None:
 
 def dinner_date_possible() -> List[models.Restaurant]:
     """You have asked someone out on a dinner date, but where to go?
+
     You want to eat at around 19:00 and your date is vegan.
     Query a list of restaurants that account for these constraints.
     """
@@ -47,6 +51,7 @@ def dinner_date_possible() -> List[models.Restaurant]:
 
 def add_dish_to_menu() -> models.Dish:
     """You have created a new dish for your restaurant and want to add it to the menu
+
     The dish you create must at the very least contain 'cheese'.
     You do not know which ingredients are in the database, but you must not
     create ingredients that already exist in the database. You may create
