51e27e58e0de3bf94ee46ad3f7b10925a248b359
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-06-06 13:32:52 +0200

    Add Code

--- stds/main.py
+++ stds/main.py
@@ -4,17 +4,28 @@ __human_name__ = "stds"
 
 # Add your code after this line
 
+import sys
 
 def main():
-    # TODO: read text from stdin
+    # Read text from stdin
+    text = sys.stdin.read().strip()
 
-    # TODO: Filter character given as an argument from the text
+    # Filter character given as an argument from the text
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <character>", file=sys.stderr)
+        return
+    
+    char = sys.argv[1]
+    filtered_text = text.replace(char, '')
 
-    # TODO: Print the result to stdout
+    # Print the result to stdout
+    print(filtered_text)
 
-    # TODO: Print the total number of removed characters to stderr
-    ...
+    # Print the total number of removed characters to stderr
+    removed_count = len(text) - len(filtered_text)
+    print(removed_count, file=sys.stderr)
 
 
 if __name__ == "__main__":
     main()
+
